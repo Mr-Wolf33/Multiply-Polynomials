a8398d7e2341c2fc1e058ad739bb260b16976b19
Mr-Wolf33 <113931526+Mr-Wolf33@users.noreply.github.com>
2025-02-25 15:44:59 -0600

    Update MultPoly.js comment

--- MultPoly.js
+++ MultPoly.js
@@ -19,7 +19,7 @@ function generateProblem() {
 function generatePolynomial(difficulty) {
     let terms = [];
     for (let i = 0; i < difficulty + 1; i++) {
-        let coefficient = Math.floor(Math.random() * 10) + 1;
+        let coefficient = Math.floor(Math.random() * 10) + 1; // Coefficents of x from 1 to 10
         let exponent = Math.floor(Math.random() * 3) + 1; // Powers of x from 1 to 3
         terms.push(`${coefficient}x^${exponent}`);
     }
